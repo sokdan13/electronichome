b61bfafb9fda890eee8504f5b8a6b6c317469635
Danila S <sokolina005@mail.ru>
2026-04-18 11:59:04 +0300

    Созданы экраны для подачи заявок и передачи показаний

---
+++ app/src/main/java/com/example/electronichome/presentation/screens/MetersScreen.kt
@@ -0,0 +1,8 @@
+package com.example.electronichome.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MetersScreen(onNavigateBack: () -> Boolean) {
+    TODO("Not yet implemented")
+}
---
+++ app/src/main/java/com/example/electronichome/presentation/screens/RequestsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.electronichome.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RequestsScreen(onNavigateBack: () -> Boolean) {
+    TODO("Not yet implemented")
+}
